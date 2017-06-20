e6862caed2c78206608aeb22a0e5a45d2de2bdcc
Alastair <alastairclegg@Alastairs-MacBook-Pro-2.local>
2017-06-20 17:02:30 +0100

    Bootstrap added to root module

--- theReservev1/src/app/app.module.ts
+++ theReservev1/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { BasicBrandHeaderComponent } from './basic-brand-header/basic-brand-header.component';
@@ -10,7 +11,8 @@ import { BasicBrandHeaderComponent } from './basic-brand-header/basic-brand-head
     BasicBrandHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
